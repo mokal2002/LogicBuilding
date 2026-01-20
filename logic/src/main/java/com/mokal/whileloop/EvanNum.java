a95061442a0dbaa55c2e5382e6aaebac8ec43fba
mokal2002 <aniketmokal29@gmail.com>
2026-01-20 13:01:14 +0530

    Print numbers from 10 down to 1 in reverse order

---
+++ logic/src/main/java/com/mokal/whileloop/EvanNum.java
@@ -0,0 +1,16 @@
+package com.mokal.whileloop;
+
+public class EvanNum {
+    public static void main(String[] args) {
+        System.out.println("Print all even numbers between 1 and 100.");
+
+        int i = 2;
+
+        while (i <= 100) {
+            System.out.println(i);
+            i += 2;
+        }
+
+        
+    }
+}
