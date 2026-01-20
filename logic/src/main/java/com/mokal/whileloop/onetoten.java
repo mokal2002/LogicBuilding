be340136dabe7692ff878d50b19d83a756074420
mokal2002 <aniketmokal29@gmail.com>
2026-01-20 12:53:03 +0530

    Print numbers from 10 down to 1 in reverse order

---
+++ logic/src/main/java/com/mokal/whileloop/onetoten.java
@@ -0,0 +1,16 @@
+package com.mokal.whileloop;
+
+public class onetoten {
+
+    public static void main(String args[]) {
+        // Printing 1 to 10 Using while loop
+        System.out.println("Print 1 to 10 using while loop.");
+        System.out.println("");
+        int i = 1;
+        while(i <= 10){
+            System.out.println("Num "  + i );
+            i++;
+        }
+    }
+
+}
---
+++ logic/src/main/java/com/mokal/whileloop/tento1.java
@@ -0,0 +1,16 @@
+package com.mokal.whileloop;
+
+public class tento1 {
+    public static void main(String[] args) {
+        System.out.println("Print numbers from 10 down to 1 in reverse order.");
+
+        int i = 10;
+        
+        while (i >= 1) {
+            System.out.println(i);
+            i--;
+
+            
+        }
+    }
+}
