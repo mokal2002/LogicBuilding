0da7e1fbac14a2dfcf247a6c8d9dee0e7fbea3a0
mokal2002 <aniketmokal29@gmail.com>
2026-01-21 20:27:23 +0530

    g

---
+++ logic/src/main/java/com/mokal/whileloop/Pali.java
@@ -0,0 +1,18 @@
+package com.mokal.whileloop;
+
+import java.util.Scanner;
+
+public class Pali {
+    public static void main(String[] args) {
+        System.out.println("Find the Given Number plaidrome or not.");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("eEnter a number to cjeack wethever its pali or not.");
+
+
+        int num = sc.nextInt();
+
+        int 
+    }
+    
+}
